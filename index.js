6b9c274030f0b769e1be4f14259723c313c4581b
SCJAB <zzep96@gmail.com>
2023-05-12 22:25:22 +0800

    update

--- index.js
+++ index.js
@@ -1,52 +1,30 @@
-var UI_Number1 = document.getElementById("UI_Number");
-var UI_Number2 = document.getElementById("UI_Number_Result");
-var result = document.getElementById("result");
+var UI_NumberValue = document.getElementById('UI_Number');
 
-function sum()
+function sum(val) 
 {
-  result.innerHtml = getResult("+");
+	return (UI_NumberValue).value += val;
 }
-function difference()
+function difference(val) 
 {
-  result.innerHtml = getResult("-");
+	return (UI_NumberValue).value += val;
 }
-function product()
+function product(val) 
 {
-  result.innerHtml = getResult("*");
+	return (UI_NumberValue).value += val;
 }
-function quotient()
+function quotient(val) 
 {
-  result.innerHtml = getResult("/");
-}
-function clear()
-{
-  result.innerHtml = getResult("c");
+	return (UI_NumberValue).value += val;
 }
 
-function getResult(operator)
+function clearCalculation() 
 {
-  var UI_NumberValue1 = parseFloat(UI_Number1.value);
-  var UI_NumberValue2 = parseFloat(UI_Number2.value);
+    document.getElementById('UI_Number').value = '';
+    document.getElementById('result').innerText = '';
+}
 
-  if (operator === "+")
-  {
-    return UI_NumberValue1 + UI_NumberValue2;
-  }
-  else if (operator === "-")
-  {
-    return UI_NumberValue1 - UI_NumberValue2;
-  }
-  else if (operator === "*")
-  {
-    return UI_NumberValue1 * UI_NumberValue2;
-  }
-  else if (operator === "/")
-  {
-    return UI_NumberValue1 / UI_NumberValue2;
-  }
-  else if (operator === "C")
-  {
-    document.getElementById("UI_Number").value = "";
-	  document.getElementById("UI_Number_Result").value = "";
-  }
+function calculateall() 
+{
+	var result = eval(document.getElementById('UI_Number').value);
+	document.getElementById('result').innerText = result;
 }
